5d8ee55fd554e876e7be7e39fb715f1d9daac37c
Joshua Dillard <joshuadillard@Joshuas-MacBook-Pro.local>
2026-04-06 06:37:25 -0500

    continuing on in main.dart file with OAuth setup.

--- lib/main.dart
+++ lib/main.dart
@@ -8,5 +8,22 @@ void main() => runApp(MaterialApp(home: YTMvp()));
 class YTMvp extends StatefulWidget {
   @override
   _YTMvpState createState() => _YTMvpState();
-  // TODO start here with understanding the stateful widget setup for the login screen
+}
+
+class _YTMvpState extends State<YTMvp> {
+  // Define scope from setup in Google Cloud
+  final GoogleSignIn _googleSignIn = GoogleSignIn(
+    scopes: [YouTubeApi.youtubeReadonlyScope],
+  );
+
+  GoogleSignInAccount? _currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    _googleSignIn.onCurrentUserChanged.listen((account) {
+      setState(() => _currentUser = account);
+    });
+    // TODO.. start here after Stream subscription
+  }
 }
